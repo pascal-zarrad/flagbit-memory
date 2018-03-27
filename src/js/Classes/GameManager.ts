4067e05d634d0e6a8dd43e7bbd4e96f286493bf9
Marius David Wieschollek <marius.wieschollek@flagbit.de>
2018-03-27 13:42:34 +0200

    Updated scenario

--- src/js/Classes/GameManager.ts
+++ src/js/Classes/GameManager.ts
@@ -32,7 +32,7 @@ export default class GameManager {
 
     constructor() {
         this._deck = [];
-        this._status = {choice: []};
+        this._status = {};
         this._timeout = null;
         this._game = {difficulty: null, startTime: null, endTime: null, turns: 0, started: false, won: false, lost: false, locked: false}
     }
@@ -44,13 +44,6 @@ export default class GameManager {
      * @param user
      */
     startGame(difficulty: string, user: string) {
-        this._game.difficulty = difficulty;
-        this._game.startTime = new Date();
-        this._game.started = true;
-
-        if (difficulty === 'hard') this._timeout = setTimeout(() => { this._looseGame() }, 60000);
-        this._shuffleDeck();
-        return true;
     }
 
     /**
@@ -58,15 +51,6 @@ export default class GameManager {
      * @private
      */
     _shuffleDeck() {
-        this._deck = [];
-        let size       = this._game.difficulty === 'easy' ? 16:36,
-            multiplier = (size / 2) - 1;
-
-        for (let i = 0; i < size; i++) {
-            let rand = this._getRandomNumber(multiplier);
-
-            this._deck.push({id: i, image: rand, show: false});
-        }
     }
 
     /**
@@ -76,23 +60,6 @@ export default class GameManager {
      * @private
      */
     _getRandomNumber(max: number) {
-        let tries = 0;
-        loop: while (tries < 256) {
-            let rand  = Math.round(Math.random() * max),
-                count = 0;
-            tries++;
-
-            for (let i = 0; i < this._deck.length; i++) {
-                if (this._deck[i].image === rand) {
-                    count++;
-                    if (count === 2) continue loop;
-                }
-            }
-
-            return rand;
-        }
-        alert('Unable to generate random number');
-        throw 'Random too many retries: ' + tries;
     }
 
     /**
@@ -102,80 +69,11 @@ export default class GameManager {
      * @param id
      */
     showCard(id: number) {
-        if (this._deck[id].show || this.game.locked) return;
-        this._game.turns++;
-        let card = this._deck[id];
-
-        if (this._status.choice.length === 2) {
-            let [card1, card2] = this._status.choice;
-            if(card1.image !== card2.image) {
-                card1.show = false;
-                card2.show = false;
-            }
-            this._status.choice = [];
-        }
-
-        card.show = true;
-        this._status.choice.push(card);
-
-        this._checkIfGameFinished()
     }
 
     /**
      *
      */
     resetGame() {
-        this._deck = [];
-        this._status = {choice: []};
-        this._game = {difficulty: null, startTime: null, endTime: null, turns: 0, started: false, won: false, lost: false, locked: false};
-    }
-
-    /**
-     * Checks if the game is finished and acts accordingly
-     *
-     * @returns {boolean}
-     */
-    _checkIfGameFinished() {
-        let allCardsVisible = true;
-        for (let i = 0; i < this._deck.length; i++) {
-            if (this._deck[i].show === false) {
-                allCardsVisible = false;
-                break;
-            }
-        }
-        if (allCardsVisible) {
-            this._winGame();
-        } else if (this._game.difficulty === 'hard' && this._game.turns > 90) {
-            this._looseGame();
-        }
-    }
-
-    /**
-     *
-     * @private
-     */
-    _winGame() {
-        if (this._game.lost) return;
-        this._endGame();
-        this._game.won = true;
-    }
-
-    /**
-     *
-     * @private
-     */
-    _looseGame() {
-        if (this._game.won) return;
-        this._endGame();
-        this._game.lost = true;
-    }
-
-    /**
-     *
-     * @private
-     */
-    _endGame() {
-        if (this._timeout) clearTimeout(this._timeout);
-        this._game.endTime = new Date();
     }
 }
